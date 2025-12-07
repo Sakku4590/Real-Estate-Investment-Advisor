1510e5224d873774e4029227da4a6412507ccac2
Sakku4590 <ssklinemustaque3@gmail.com>
2025-12-07 19:10:52 +0530

    Create data_ingestion.py file

---
+++ src/data_ingestion.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+import logging
+
+
+# Ensure the log directory exiest
+log_dir = 'logs'
+os.makedirs(log_dir,exist_ok=True)
+
+
+# login configuration
+logger = logging.getLogger('data_ingestion')
+logger.setLevel('DEBUG')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel('DEBUG')
+
+log_file_path = os.path.join(log_dir,'data_ingestion.log')
+file_handler = logging.FileHandler(log_file_path)
+file_handler.setLevel('DEBUG')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+def load_data(data_url: str) ->dict:
+    """Load Parameters from a YAML file"""
+    try:
+        df = pd.read_csv(data_url)
+        logger.debug('Data loded from %s',data_url)
+        return df
+    except pd.errors.ParserError as e:
+        logger.error('Failled to parse the CSV file: %s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occure while loading the data: %s', e)
+        raise
+    
+# def load_data(data_url: str) -> pd.DataFrame:
+#     """Load data from a CSV file"""
+#     try:
+#         df = pd.read_csv(data_url)
+#         logger.debug('Data loaded from %s',data_url)
+#         return df
+#     except pd.errors.ParserError as e:
+#         logger.error('Failed to parse the CSV file: %s',e)
+#         raise
+#     except Exception as e:
+#         logger.error('Unexpected error occured wile loading the data: %s',e)
+#         raise
+    
+def preprocess_data(df:pd.DataFrame) -> pd.DataFrame:
+    """Preprocess the data"""
+    try:
+        logger.debug('Data preprocessing complet')
+        return df
+    except KeyError as e:
+        logger.error('Missing column in the dataframe: %s',e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error during preprocessing: %s',e)
+        raise
+
+def save_data(train_data:pd.DataFrame, test_data: pd.DataFrame, data_path: str) -> None:
+    """Save the train and test dataset."""
+    try:
+        raw_data_path = os.path.join(data_path,'raw')
+        os.makedirs(raw_data_path,exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path,"train.csv"),index=False)
+        test_data.to_csv(os.path.join(raw_data_path,"test.csv"),index=False)
+        logger.debug('Train and test data occure while saving the data; %s',raw_data_path)
+    except Exception as e:
+        logger.error('Unexpected error occurred while saving the data: %s',e)
+        
+def main():
+    try:
+        test_size = 0.2
+        data_path = 'https://raw.githubusercontent.com/Sakku4590/Real-Estate-Investment/refs/heads/main/Data/india_housing_prices.csv'
+        df = load_data(data_url=data_path)
+        final_df = preprocess_data(df)
+        train_data,test_data = train_test_split(final_df,test_size=test_size,random_state=2)
+        save_data(train_data,test_data,data_path='./data')
+    except Exception as e:
+        logger.error('Failed to complete the data ingestion process: %s',e)
+
+if __name__=='__main__':
+    main()
